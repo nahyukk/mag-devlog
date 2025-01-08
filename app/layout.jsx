a5ee2d4d5dfffc68850929fbbd8cbf5851f5325e
nahyun <knahyuk@gmail.com>
2025-01-08 19:51:53 +0900

    💄 modified general CSS

--- app/layout.jsx
+++ app/layout.jsx
@@ -4,8 +4,14 @@ import "./globals.css";
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Baloo+2&family=Roboto+Mono&display=swap"
+          rel="stylesheet"
+        />
+      </head>
       <body>
-				<Navbar />
+        <Navbar />
         <main>{children}</main>
       </body>
     </html>
