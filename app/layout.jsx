83437bbc072004c0ce8b149f9d01fc1d3cc353b4
nahyun <knahyuk@gmail.com>
2025-01-10 01:06:41 +0900

    💄general - added font weight

--- app/layout.jsx
+++ app/layout.jsx
@@ -6,7 +6,7 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <head>
         <link
-          href="https://fonts.googleapis.com/css2?family=Baloo+2&family=Roboto+Mono&display=swap"
+          href="https://fonts.googleapis.com/css2?family=Baloo+2:wght@400;500;600;700;800&family=Roboto+Mono&display=swap"
           rel="stylesheet"
         />
       </head>
