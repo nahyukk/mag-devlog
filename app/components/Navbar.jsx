6f15852a3976d27652e3cf826688056982dbcbc4
nahyun <knahyuk@gmail.com>
2025-01-10 01:07:55 +0900

    💄navbar - added active state style

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -1,21 +1,27 @@
+"use client";
+
+import { usePathname } from "next/navigation";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 import styles from "./Navbar.module.css";
 
 const Navbar = () => {
+	const currentPath = usePathname();
+	console.log("Current Path:", currentPath) // 디버깅용
+	
   return (
     <nav className={styles.nav}>
       <div className={styles.navContainer}>
         <div className={styles.navContainerLeft}>
           <div className={styles.navItem}>
-            <Link href="/TIL">TIL</Link>
+            <Link href="/TIL" className={`${currentPath === "/TIL" ? styles.active : ""}`}>TIL</Link>
           </div>
           <div className={styles.navItem}>
-            <Link href="/Study">STUDY</Link>
+            <Link href="/Study" className={`${currentPath === "/Study" ? styles.active : ""}`}>STUDY</Link>
           </div>
           <div className={styles.navItem}>
-            <Link href="/Projects">PROJECTS</Link>
+            <Link href="/Projects" className={`${currentPath === "/Projects" ? styles.active : ""}`}>PROJECTS</Link>
           </div>
         </div>
         <div className={styles.navContainerRight}>
