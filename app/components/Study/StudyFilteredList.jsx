2cba1779c281f363d89d6bd7c20238cf0f38ff34
nahyun <knahyuk@gmail.com>
2025-01-30 12:47:29 +0900

    💄Study - added svg icons to filtering buttons

--- app/components/Study/StudyFilteredList.jsx
+++ app/components/Study/StudyFilteredList.jsx
@@ -6,6 +6,8 @@ import StudyList from "./StudyList";
 const StudyFilteredList = ({ filters, posts }) => {
   const [filteredPosts, setFilteredPosts] = useState(posts);
   const [selectedFilter, setSelectedFilter] = useState("All");
+  const getFilterIcon = (filter) =>
+    `/filtericons/${filter.toLowerCase()}-icon.svg`;
 
   const handleFilterClick = (filter) => {
     setSelectedFilter(filter);
@@ -19,31 +21,53 @@ const StudyFilteredList = ({ filters, posts }) => {
   return (
     <div>
       <div style={{ display: "flex", gap: "10px" }}>
-        {filters.map((filter) => (
-          <button
-            key={filter}
-            onClick={() => handleFilterClick(filter)}
-            style={{
-              padding: "5px 10px",
-              borderRadius: "15px",
-              border: "1px solid var(--primary-color)",
-              backgroundColor:
-                selectedFilter === filter
-                  ? "var(--primary-color)"
-                  : "#ffffff",
-              color:
-                selectedFilter === filter
-                  ? "#ffffff"
-                  : "var(--main-text-color)",
-              fontWeight: selectedFilter === filter ? "bold" : "400",
-              cursor: "pointer",
-              fontFamily: "var(--font-roboto-mono)",
-              fontSize: "16px",
-            }}
-          >
-            {filter}
-          </button>
-        ))}
+        {filters.map((filter) => {
+          return (
+            <button
+              key={filter}
+              onClick={() => handleFilterClick(filter)}
+              style={{
+                padding: "5px 10px",
+                borderRadius: "15px",
+                border: "1px solid var(--primary-color)",
+                backgroundColor:
+                  selectedFilter === filter
+                    ? "var(--primary-color)"
+                    : "#ffffff",
+                color:
+                  selectedFilter === filter
+                    ? "#ffffff"
+                    : "var(--main-text-color)",
+                fontWeight: selectedFilter === filter ? "bold" : "400",
+                cursor: "pointer",
+                fontFamily: "var(--font-roboto-mono)",
+                fontSize: "16px",
+              }}
+            >
+              <div
+                style={{
+                  display: "flex",
+                  alignItems: "center",
+                  gap: "3px",
+                }}
+              >
+                {!["All", "Project"].includes(filter) && (
+                  <img
+                    src={getFilterIcon(filter)}
+                    alt={`${filter} icon`}
+                    width={16}
+                    height={16}
+                    style={{
+                      verticalAlign: "middle",
+                      filter: selectedFilter === filter ? "invert(1)" : "none",
+                    }}
+                  />
+                )}
+                {filter}
+              </div>
+            </button>
+          );
+        })}
       </div>
       <StudyList posts={filteredPosts} />
     </div>
