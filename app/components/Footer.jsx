126af5243ec9eb95bf23214eaadf1123bbb34795
nahyun <knahyuk@gmail.com>
2025-02-06 23:14:42 +0900

    ✨add footer feature - include detect dark mode class to change SVG color, add GitHub link to icon, email icon to modal, and clipboard copy feat

---
+++ app/components/Footer.jsx
@@ -0,0 +1,111 @@
+"use client";
+import Link from "next/link";
+import React, { useEffect, useRef, useState } from "react";
+import styles from "@/page.module.css";
+import Image from "next/image";
+
+const Footer = () => {
+  const [isDarkMode, setIsDarkMode] = useState(false);
+  const [copySuccess, setCopySuccess] = useState(false);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const modalRef = useRef(null);
+
+  useEffect(() => {
+    const htmlElement = document.documentElement;
+    const isDark = htmlElement.classList.contains("dark");
+
+    setIsDarkMode(isDark);
+
+    const observer = new MutationObserver(() => {
+      setIsDarkMode(htmlElement.classList.contains("dark"));
+    });
+
+    observer.observe(htmlElement, {
+      attributes: true,
+      attributeFilter: ["class"],
+    });
+
+    return () => observer.disconnect();
+  }, []);
+
+  const copyIcon = isDarkMode ? "/copy-icon.svg" : "/copy-white-icon.svg";
+  const emailIcon = isDarkMode ? "/email-white-icon.svg" : "/email-icon.svg";
+  const githubIcon = isDarkMode ? "/github-white-icon.svg" : "/github-icon.svg";
+
+  const email = "knahyuk@gmail.com";
+
+  const copyToClipboard = async () => {
+    try {
+      await navigator.clipboard.writeText(email);
+      setCopySuccess(true);
+      setTimeout(() => setCopySuccess(false), 1000);
+    } catch (err) {
+      console.error("클립보드 복사 실패:", err);
+    }
+  };
+
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (modalRef.current && !modalRef.current.contains(event.target)) {
+        setIsModalOpen(false);
+      }
+    };
+
+    if (isModalOpen) {
+      document.addEventListener("mousedown", handleClickOutside);
+    } else {
+      document.removeEventListener("mousedown", handleClickOutside);
+    }
+
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [isModalOpen]);
+
+  return (
+    <div className={styles.footerContainer}>
+      {isModalOpen && (
+        <div
+          className={styles.modalOverlay}
+          onClick={() => setIsModalOpen(false)}
+        >
+          <div
+            ref={modalRef}
+            className={styles.modalContent}
+            onClick={(e) => e.stopPropagation()}
+          >
+            <p style={{ fontWeight: "bold", marginBottom: "2px" }}>
+              Contact Me!
+            </p>
+            <div className={styles.modalEmail}>
+              <p>{email}</p>
+              <button className={styles.copyButton} onClick={copyToClipboard}>
+                <Image src={copyIcon} alt="Copy Icon" width={20} height={20} />
+              </button>
+              {copySuccess && (
+                <span className={styles.copySuccess}>복사됨!</span>
+              )}
+            </div>
+          </div>
+        </div>
+      )}
+      <div className={styles.footerLinks}>
+        <Link href="https://github.com/nahyukk" target="_blank">
+          <Image src={githubIcon} alt="GitHub Icon" width={30} height={30} />
+        </Link>
+        <button
+          className={styles.emailButton}
+          onClick={() => setIsModalOpen(true)}
+        >
+          <Image src={emailIcon} alt="Email Icon" width={30} height={30} />
+        </button>
+      </div>
+      <div className={styles.footerText}>
+        <p>Copyright © 2025 NahyunKim</p>
+        <p>Mag.dev</p>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
