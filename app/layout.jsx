a7e35984a6de0b57822a9d21ed4aa6093a5df9ff
nahyun <knahyuk@gmail.com>
2025-02-06 23:15:36 +0900

    ✨apply footer to layout

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,3 +1,4 @@
+import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
 import "./globals.css";
 
@@ -13,6 +14,7 @@ export default function RootLayout({ children }) {
       <body>
         <Navbar />
         <main>{children}</main>
+        <Footer />
       </body>
     </html>
   );
