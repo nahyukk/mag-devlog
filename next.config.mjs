1cb908f83eec62e38dd692422b354eeb4ff0c4e8
nahyun <knahyuk@gmail.com>
2025-02-12 18:39:49 +0900

    🐛add code to separate local development and deployment environments

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig = {
-  output: "export",
+  ...(isProd && { output: "export" }),
   basePath: "/til-and-study",
   assetPrefix: "/til-and-study",
 };
