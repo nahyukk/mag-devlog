3dfe4b0df0efcaf76dd3a70e66a8ed0d402d63c6
Mag :) <knahyuk@gmail.com>
2025-08-05 14:42:18 +0900

    🚧 update next-sitemap config

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: "https://mag-devlog.vercel.app/",
+  siteUrl: "https://mag-devlog.vercel.app",
   generateRobotsTxt: true,
   sitemapSize: 5000,
   outDir: "./out",
