43fffcee9dad093d1e30bfe2e6c0f21f0bddd143
Mag :) <knahyuk@gmail.com>
2025-08-05 14:32:14 +0900

    🚧 update next-sitemap config to exclude hidden routes

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -3,4 +3,12 @@ module.exports = {
   siteUrl: "https://mag-devlog.vercel.app/",
   generateRobotsTxt: true,
   sitemapSize: 5000,
+  outDir: "./out",
+  exclude: [
+    "/portfolio",
+    "/TIL",
+    "/Study",
+    "/TIL/*",
+    "/Study/*",
+  ],
 };
